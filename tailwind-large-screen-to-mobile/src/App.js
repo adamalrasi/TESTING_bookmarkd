2d197bd511436a754b1e935710debf7d11194293
Adam <adam.alrasi@outlook.com>
2023-12-03 21:51:30 +0000

    first commit

---
+++ tailwind-large-screen-to-mobile/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Headline</h1>
+      <p>random</p>
+      <p>random</p>
+      <p>random</p>
+      <p>random</p>
+      <p>random</p>
+    </>
+  );
+}
+
+export default App;
